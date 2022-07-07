d5b2099e0b066f88a4e263c15cc7c47eac061be2
Adriel <adr.fr.27101992@gmail.com>
2022-07-07 00:45:42 -0300

    comunicação com 'API' adcionada

--- scripts/tarefas.js
+++ scripts/tarefas.js
@@ -1,45 +1,43 @@
-/*
-Ver tarefas pendentes.
-
-Ver tarefas terminadas.
-
-Marcar uma tarefa como terminada.
-
-Criar tarefas novas. OK!
-
-Visualizar a data de criação de uma tarefa.
-
-Remover tarefas.
-*/
 const logoffButton = document.querySelector("#closeApp")
 logoffButton.addEventListener("click", logoff)
 
 const form = document.querySelector("form")
-form.addEventListener("submit",  createTask)
+form.addEventListener("submit",  handleSubmit)
 
 const valorInput = document.querySelector("#novaTarefa")
 const listFinishedTasks = document.querySelector(".tarefas-terminadas")
 const listTasks = document.querySelector(".tarefas-pendentes")
 listTasks.innerHTML = ""
 
-function createTask(evento){
-    evento.preventDefault()
+function handleSubmit(evento){
+  evento.preventDefault()
+  saveTask({description:valorInput.value, completed: false})
+}
+
+function saveTask(task){
+  registerTask(task)
+  console.log(task, "task")
+}
+
+function createTask(task){
     const li = document.createElement("li")
     li.classList.add("tarefa")
     li.innerHTML = 
     `<div class="not-done"></div>
       <div class="descricao">
-        <p class="nome"> ${valorInput.value} </p>
-        <p class="timestamp">Criada em: ${new Date()}</p>
+        <p class="nome"> ${task.description} </p>
+        <p class="timestamp">Criada em: ${task.createdAt}</p>
         <button>
           <img src="./assets/deletar.png" alt="">
         </button>
       </div>
     `
     const dot = li.childNodes[0]
+    li.addEventListener("dblclick" , () => deleteTask(task.id, li))
     dot.addEventListener('click', () => completeTask(li))
     listTasks.appendChild(li)
 }
+
 
 function completeTask(elemento) {
   const dot = elemento.childNodes[0]
@@ -57,17 +55,77 @@ function undoCompleted(elemento) {
   listTasks.appendChild(elemento)
 }
 
+
+function removeTask(elemento){
+  console.log(elemento)
+
+}
+
+
 function logoff() {
   localStorage.removeItem('jwt')
   window.location.href = 'index.html'
   console.log('Aooba')
 }
 
+async function registerTask(task) {
+  const token = localStorage.getItem("jwt")
+  if (!token) return;
+  const options = {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'authorization': token.replaceAll('"', '')
+      },
+      body: JSON.stringify(task)
+  }
 
-/*function removeTask(){
-
+  await fetch("https://ctd-todo-api.herokuapp.com/v1/tasks", options)
+      .then((res) => res.json())
+      .then((data) => createTask(data))
+      .catch((err) => console.error(err.message))
 }
 
-function finishTask(){
+async function getTask() {
+  const token = localStorage.getItem("jwt")
+  if (!token) return;
+  const options = {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json',
+        'authorization': token.replaceAll('"', '')
+      },
+  }
 
-}*/
+  await fetch("https://ctd-todo-api.herokuapp.com/v1/tasks", options)
+      .then((res) => res.json())
+      .then((tasks) => tasks.forEach(task => createTask(task)))
+      .catch((err) => console.error(err.message))
+}
+
+async function deleteTask(id, elemento) {
+  const token = localStorage.getItem("jwt")
+  if (!token) return;
+  elemento.remove()
+  const options = {
+      method: 'DELETE',
+      headers: {
+        'content-type': 'application/json',
+        'authorization': token.replaceAll('"', '')
+      },
+  }
+
+  await fetch("https://ctd-todo-api.herokuapp.com/v1/tasks/" + id, options)
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error(err.message))
+}
+
+getTask()
+
+
+/*
+registerTask({
+  "description": "Aprender Javascript",
+  "completed": false
+})*/
