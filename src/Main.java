bf46c527c88dbd9ef47dd23f3eb007b2f6ca9c7b
Gabriel Henrique <gabriel.henrique@agenciam3.com>
2022-10-20 20:51:24 -0300

    :tada: finished

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,13 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ServicoDownloadProxy servicoDownloadProxy = new ServicoDownloadProxy();
+		Usuario user1 = new Usuario("gratis");
+		Usuario user2 = new Usuario("premium");
+		
+		servicoDownloadProxy.download(user1);
+		System.out.println(" ------------ ");
+		servicoDownloadProxy.download(user2);
 	}
 
 }
